d0a2e36bd37cfed77c30a3c4ebadedd816634cf0
andri-003 <alhaqandriawan@gmail.com>
2023-03-12 14:41:30 +0700

    -fix = add file function.js -fix = update file index

---
+++ function.js
@@ -0,0 +1,10 @@
+function say(){
+    console.log('selamat pagi');
+    console.log('sugeng enjing');
+    return "samlikum";
+    console.log('spada');
+}
+
+// let salam = say();
+// console.log(salam);
+
