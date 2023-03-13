71e66d7e5f94547940ec240993ed0435ffcf388e
andriawan96 <alhaqandriawan@gmail.com>
2023-03-13 10:31:46 +0700

    -fix = update funcrion.js

--- function.js
+++ function.js
@@ -8,3 +8,11 @@ function say(){
 // let salam = say();
 // console.log(salam);
 
+// perulangan for
+// for(let i = 1; i <=20; i += 2){
+//     console.log('dalam loop ke ' + i);
+// }
+
+for(let i = 30; i >=0; i -= 3){
+    console.log('dalam loop ke ' + i);
+}
