cc7f18822592b0428ac5646b458993c81f9ef2a1
andriawan96 <alhaqandriawan@gmail.com>
2023-03-14 06:54:49 +0700

    -fix = update learn nested loop

--- function.js
+++ function.js
@@ -13,6 +13,20 @@ function say(){
 //     console.log('dalam loop ke ' + i);
 // }
 
-for(let i = 30; i >=0; i -= 3){
-    console.log('dalam loop ke ' + i);
+// for(let i = 30; i >=0; i -= 3){
+//     console.log('dalam loop ke ' + i);
+// }
+
+// const alat = ['pc', 'printer', 'monitor','stop kontak', 'router'];
+// for( let i = 0; i < alat.length; i++){
+//     console.log(i + 1, alat[i]);
+// }
+
+// nested loop
+const pilihan = 'ABCD';
+for(let i = 1; i <= 10; i++){
+    console.log(`${i}. Soal nomor ${i}:`);
+    for(let j = 0; j < pilihan.length; j++){
+        console.log(`  ${pilihan[j]}. Pilihan Jawaban`);
+    }
 }
