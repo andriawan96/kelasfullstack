737cf0ace38e4f0a6b9f72285e833d0fac051209
andriawan96 <alhaqandriawan@gmail.com>
2023-03-18 22:53:22 +0700

    -fix = update function

--- function.js
+++ function.js
@@ -32,7 +32,58 @@ function say(){
 // }
 
 // nested array loop
-const  studentRwo = [
-    ['dodo', 'didi', 'dudu'],
-    ['']
-]
+// const  studentRow = [
+//     ['dodo', 'didi', 'dudu'],
+//     ['kaka', 'kiki', 'kuku'],
+//     ['nara','niri','nuru'],
+// ];
+
+// console.log(studentRow);
+
+let situs = 'bing';
+// switch(situs){
+//     case 'google': console.log('ini situs google'); break;
+//     case 'yahoo': console.log('ini situs yahoo'); break;
+//     case 'bing': console.log('ini situs bing'); break;
+//     default: console.log('tidak ada situs yg di pilih');
+// }
+
+let jumlah_barang = 100;
+let total;
+
+// if (jumlah_barang > 500){
+//     total = jumlah_barang * 100;
+// } else {
+//     total = jumlah_barang * 150;
+// }
+
+total = jumlah_barang > 50 ? jumlah_barang * 20 : jumlah_barang * 30;
+// console.log(total);
+
+let user = 'adminn';
+// let hak_akses = user === 'admin'? true : false ;
+// if(hak_akses){
+//     console.log("selamat datang admin ...");
+// } else {
+//     console.log('anda bukan admin');
+// }
+
+// for ( let i = 1; i <= 20; i = i + 2){
+//     console.log(`hello ke ${i}`)
+// }
+// let i;
+// let j;
+// let x;
+// for( i = 1; i <= 4; i++){
+//     for( j = 1; j <= 3; j++){
+//         for( x = 1; x <= 2; x++){
+//                     console.log(`variabel i bernilai ${i} , variabel j bernilai ${j} , variabel x bernilai ${x}`);
+//         }
+//     }
+// }
+
+let i = 100;
+while ( i > 0){
+    console.log( i + " * 5 = " + i*5);
+    i = i -5;
+}
