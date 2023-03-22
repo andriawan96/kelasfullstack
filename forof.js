ffa735ebb165b3a5565217c93bb76adf24c61415
andri-003 <alhaqandriawan@gmail.com>
2023-03-22 09:38:07 +0700

    -fix = update forof dan forin

--- forof.js
+++ forof.js
@@ -4,12 +4,29 @@
 //     console.log(`Buah ${listBuah}`);
 // }
 
-const daftarBuah = [ ['pisang', 'nangka', 'jambu', ],
-                    ['manggis', 'kecapi', 'mangga'],
-                    ['sirsak', 'markisa', 'jamblang'] ];
+// const daftarBuah = [ ['pisang', 'nangka', 'jambu', ],
+//                     ['manggis', 'kecapi', 'mangga'],
+//                     ['sirsak', 'markisa', 'jamblang'] ];
 
-for ( let row of daftarBuah) {
-    for ( let buahBuahan of row ) {
-        console.log(buahBuahan);
-    }
+// for ( let row of daftarBuah) {
+//     for ( let buahBuahan of row ) {
+//         console.log(buahBuahan);
+//     }
+// }
+
+const studentScore = {
+    dodo: 20,
+    aji: 19,
+    pais: 18,
+    harbi: 17
+};
+for (let student in studentScore){
+    console.log(`Siswa ${student} memiliki nilai/skor ${studentScore[student]}`);
 }
+
+let total = 0;
+let scores = Object.values(studentScore);
+for (let score of scores){
+    total += score;
+} 
+console.log("Nilai rata-rata : " + total / scores.length);
