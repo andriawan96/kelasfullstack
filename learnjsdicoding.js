6deb660fe9188e07485b854ee8d5789bed356d86
andri-003 <alhaqandriawan@gmail.com>
2023-04-06 23:59:30 +0700

    - fix = update learnjsdicodi

--- learnjsdicoding.js
+++ learnjsdicoding.js
@@ -78,4 +78,117 @@ const mainan = ['tamiya', 'RC', 'congklak', 'gambaran', 'tokdal'];
 const game = ['bola', 'berenang', 'manjat pohon']
 const allFavorit = [...mainan, ...game];
 // console.log(...mainan);
-console.log(allFavorit);
+// console.log(allFavorit);
+
+// destructuring object
+// const profil = {
+//     firstname: "john",
+//     lastname: "du",
+//     age: 26
+// }
+
+// let firstname = "joe";
+// let age = 20;
+
+// const {firstname, lastname, age} = profil;
+
+// console.log(`Nama depan : ${firstname} nama belakang : ${lastname} usia : ${age}`);
+// console.log(firstname, lastname, age);
+
+const profil = {
+    firstname: "john",
+    lastname: "du",
+    age: 26
+}
+
+// let firstname = "joe";
+// let age = 20;
+
+// ({firstname, age} = profil);
+// console.log(firstname);
+// console.log(age);
+
+
+/*function init() {
+    const name = 'wan andri';
+
+    function greet() {
+        console.log(`Halo, ${name}`);
+    }
+
+    greet();
+}*/
+// init();
+
+/*const add = () => {
+    let counter = 0;
+    return () => { return ++counter; };
+}
+
+const addCounter = add();
+console.log(addCounter());
+console.log(addCounter());
+console.log(addCounter()); */
+
+//kuiz
+/*function minimal(a,b) {
+    if ( a < b) {
+        return a;
+    } else if(b < a) {
+        return b;
+    } else {
+        return a;
+    }
+}
+
+console.log(minimal(1,4));
+console.log(minimal(3,2));
+console.log(minimal(3,3));
+
+function findIndex(array, number) {
+    for(let i = 0; i < array.length; i++)
+    if(array[i]===number){
+        return[i];
+    }
+    return -1;
+}*/
+// console.log(findIndex([1,2,3,4,5],3));
+// console.log(findIndex([1,2,3,4,5],6));
+// console.log(findIndex([1,2,3,4,5],5));
+
+
+//============
+function minimal (a,b) {
+  if(a === b){
+    return a;
+  } else if (a < b){
+    return a;
+  } else{
+    return b;
+  }
+}
+
+// console.log(minimal(1,4));
+// console.log(minimal(3,2));
+// console.log(minimal(3,3));
+
+function findIndex(array, number) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i]===number){
+            return[i];
+        }
+    }
+    return -1;
+}
+// console.log(findIndex([1,2,3,4,5],3));
+// console.log(findIndex([1,2,3,4,5],6));
+// console.log(findIndex([1,2,3,4,5],5));
+
+
+function multiply(num) {
+    total = num * num;
+}
+
+const result = multiply(3);
+
+console.log(result);
