1969f435d3e5627a582411df81f14389d766f786
andri-003 <alhaqandriawan@gmail.com>
2023-04-08 00:38:10 +0700

    - new = add src oop in index - new = add file oop.js

---
+++ oop.js
@@ -0,0 +1,68 @@
+/*contoh membuat object blueprint dari sebuah mobil*/
+/*function Car(brand, color, maxSpeed, chassisNumber){
+    this.brand = brand;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+    this.chassisNumber = chassisNumber;
+}*/
+
+// Anda bisa membuat objek mobil--yang merupakan instance dari Car--dengan memanfaatkan constructor function Car
+/*Car.prototype.drive = function() {
+    console.log(`${this.brand} ${this.color} is driving`);
+}
+
+Car.prototype.reverse = function() {
+    console.log(`${this.brand} ${this.color} is reverse`);
+}
+
+Car.prototype.turn = function() {
+    console.log(`${this.brand} ${this.color} is turning`);
+}
+
+// Membuat objek mobil dengan constructor function Car
+const car1 = new Car('Kijang Inova', 'Grey', 2000, 'to-1');
+const car2 = new Car('Avanza', 'Grey', 1200, 'to-2');
+const car3 = new Car('Calya', 'Grey', 1000, 'to-3');
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
+car1.reverse();
+car2.drive();
+car3.turn(); */
+
+//jika menggunakan class
+class Car {
+  constructor(brand, color, maxSpeed, chassisNumber) {
+    this.brand = brand;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+    this.chassisNumber = chassisNumber;
+  }
+ 
+  drive() {
+    console.log(`${this.brand} ${this.color} is driving`);
+  }
+ 
+  reverse() {
+    console.log(`${this.brand} ${this.color} is reversing`);
+  }
+ 
+  turn() {
+    console.log(`${this.brand} ${this.color} is turning`);
+  }
+}
+ 
+// Membuat objek mobil dengan constructor function Car
+const car1 = new Car('Toyota', 'Silver', 200, 'to-1');
+const car2 = new Car('Honda', 'Black', 180, 'ho-1');
+const car3 = new Car('Suzuki', 'Red', 220, 'su-1');
+ 
+console.log(car1);
+console.log(car2);
+console.log(car3);
+ 
+car1.drive();
+car2.drive();
+car3.drive();
